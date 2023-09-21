f0da7804d91f1669f43bd54e3ced069da186b92c
Sandro <sandrocampo210@gmail.com>
2023-09-21 20:16:22 +0200

    App bar is fully functional and now the settings screen is working too.

--- app/src/main/java/com/example/opscpoe/SettingsFragment.kt
+++ app/src/main/java/com/example/opscpoe/SettingsFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment() {
@@ -12,6 +16,34 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings, container, false)
+        val view = inflater.inflate(R.layout.settings, container, false)
+
+        val unitRadioGroup: RadioGroup = view.findViewById(R.id.unitRadioGroup)
+        val distanceSeekBar: SeekBar = view.findViewById(R.id.distanceSeekBar)
+        val distanceTextView: TextView = view.findViewById(R.id.distanceTextView)
+
+        // Initialize with km
+        distanceTextView.text = "Max Distance: 0 km"
+
+        // Update based on radio button selection
+        unitRadioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val unit = if (checkedId == R.id.metricRadioButton) "km" else "miles"
+            val currentProgress = distanceSeekBar.progress
+            distanceTextView.text = "Max Distance: $currentProgress $unit"
+        }
+
+        // Update based on seek bar movement
+        distanceSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val unit = if (view.findViewById<RadioButton>(R.id.metricRadioButton).isChecked) "km" else "miles"
+                distanceTextView.text = "Max Distance: $progress $unit"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        return view
     }
 }
